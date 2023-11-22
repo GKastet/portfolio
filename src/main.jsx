d3f2e9b46f1ab9772de8b81ae14e2d75f0271abe
GKastet <127600582+GKastet@users.noreply.github.com>
2023-11-22 20:40:20 +0100

    browser router

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,12 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
-      <App />
-    </BrowserRouter>
+  // <BrowserRouter basename={import.meta.env.BASE_URL}>
+
+  //   <App />
+  // </BrowserRouter>
+  <BrowserRouter basename="/portfolio">
+    <App />
+  </BrowserRouter>
   // </React.StrictMode>
 );
