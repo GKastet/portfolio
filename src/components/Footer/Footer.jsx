019b7641f4e9f2ffcee0e0997757f7918ba0f107
GKastet <127600582+GKastet@users.noreply.github.com>
2023-11-27 15:25:27 +0100

    footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,81 @@
-import { FooterS } from "./FooterStyled";
+import { FaLinkedin, FaTelegram, FaGithub } from "react-icons/fa";
+import { MdPermPhoneMsg } from "react-icons/md";
+import { CgMail } from "react-icons/cg";
+import { contacts } from "../../components/helpers/contacts";
+import {
+  BottomTextBox,
+  FooterBox,
+  FooterS,
+  GitBox,
+  GitLink,
+  IconListUl,
+  SocialBox,
+  SocialLink,
+} from "./FooterStyled";
 
 const Footer = () => {
+  const { phone, email, telegram, gitHub, linkedIn } = contacts;
   return (
     <FooterS>
-      <div>
-        <p>Всі права захищені &copy; Lex, 2020</p>
-      </div>
+      <FooterBox>
+        <SocialBox>
+          <h3>My contact links:</h3>
+          <IconListUl>
+            <li>
+              <SocialLink
+                to={`${linkedIn.linkedInLink}`}
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <FaLinkedin />
+              </SocialLink>
+            </li>
+            <SocialLink
+              to={`tel:${phone.number}`}
+              type="phone"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <MdPermPhoneMsg />
+            </SocialLink>
+            <SocialLink
+              to={`${telegram.telegram}`}
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <FaTelegram />
+            </SocialLink>
+            <SocialLink to={`mailto:${email.email}`}>
+              <CgMail />
+            </SocialLink>
+          </IconListUl>
+        </SocialBox>
+        <GitBox>
+          <h3>More projects I’ve worked on</h3>
+          <GitLink
+            to={`${gitHub.gitHubLink}`}
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <FaGithub />
+            <p>@GKastet on GitHub</p>
+          </GitLink>
+        </GitBox>
+      </FooterBox>
+      <BottomTextBox>
+        <p>&copy; All rights reserved, KG, 2023</p>
+        <p>
+          Designed by
+          <a
+            href="mailto:pkmurungi.pm@gmail.com"
+            type="email"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            pkmurungi.pm
+          </a>
+        </p>
+      </BottomTextBox>
     </FooterS>
   );
 };
--- src/components/Footer/FooterStyled.js
+++ src/components/Footer/FooterStyled.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const FooterS = styled.footer`
@@ -5,20 +6,107 @@ export const FooterS = styled.footer`
   width: 100%;
   height: 444px;
   display: flex;
+  flex-direction: column;
   justify-content: center;
+  align-items: center;
   background-color: var(--background-main);
   background-color: transparent;
   /* background-image: linear-gradient(to bottom, var(--background-header) , var(--background-main) 50%); */
   background-image: url("../../../imgs/wave.svg");
   /* background-image: url("../../../imgs/wave-mobile.svg"); */
-  
   background-repeat: no-repeat;
   background-size: cover;
   background-position: 50% 0%;
-  
   /* background-image: linear-gradient(
     to bottom,
     var(--background-main),
     var(--background-header)    
   ); */
+  color: var(--text-color-white);
+`;
+
+export const FooterBox = styled.div`  
+  width: 100%;
+  max-width: 910px;
+  height: 100px;
+  margin-top: 170px;
+  margin-bottom: 100px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const SocialBox = styled.div`  
+  width: 300px;
+  h3 {
+    margin-bottom: 34px;
+    font-size: 20px;
+    font-weight: 500;
+  }
+`;
+
+export const IconListUl = styled.ul`
+  display: flex;
+  justify-content: space-around;
+`;
+
+export const SocialLink = styled(Link)`
+  width: 30px;
+  height: 30px;
+  svg {
+    fill: var(--fill-color-white);
+    color: var(--fill-color-white);
+    width: 100%;
+    height: 100%;
+    transition: fill var(--transition-general), color var(--transition-general);
+    &:hover {
+      fill: var(--fill-color-red);
+      color: var(--fill-color-red);
+    }
+  }
+`;
+
+export const GitBox = styled.div`  
+  width: 300px;
+  h3 {
+    margin-bottom: 34px;
+    font-size: 20px;
+    font-weight: 500;
+  }
+`;
+
+export const GitLink = styled(Link)`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  color: var(--text-color-white);
+  fill: var(--fill-color-white);
+  transition: fill var(--transition-general), color var(--transition-general);
+  &:hover {
+    fill: var(--fill-color-red);
+    color: var(--fill-color-red);
+  }
+  svg {
+    width: 30px;
+    height: 30px;
+  }
+`;
+
+export const BottomTextBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  p {
+    display: inline-flex;
+    gap: 10px;
+    font-size: 12px;
+    letter-spacing: 1px;
+    a{
+      color: var(--text-color-white);
+      text-decoration: underline;
+      transition: color var(--transition-general);
+      &:hover{
+        color: var(--text-color-red);
+      }
+    }
+  }
 `;
